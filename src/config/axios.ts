70c40a25a4aa73645da980b9362d7d4077d421f0
Shalini Sinha <122859073+Shalinisinha22@users.noreply.github.com>
2025-08-25 21:32:22 +0530

    Update axios.ts

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api',
-  // baseURL:'https://ewa-back.vercel.app/api',
+  // baseURL: 'http://localhost:5000/api',
+  baseURL:'https://ewa-back.vercel.app/api',
   headers: {
     'Content-Type': 'application/json'
   }
